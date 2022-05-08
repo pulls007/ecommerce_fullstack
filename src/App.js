9c2084c6431dddf60775e5e2c70ab5e20c3aa4f6
pulkitdahuja <puludahuja@gmail.com>
2022-05-08 05:58:55 +0530

    intial setup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
+import { Switch, Route } from 'react-router-dom';
+import Home from './Home';
+import Login from './auth/Login';
+import Register from './auth/Register';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      {/* <header className='App-header'>
+        <p>recat app</p>
+      </header> */}
+      <Switch>
+        <Route exact path='/' component={Home}></Route>
+        <Route exact path='/login' component={Login}></Route>
+        <Route exact path='/register' component={Register}></Route>
+      </Switch>
     </div>
   );
 }
---
+++ src/auth/Login.js
@@ -0,0 +1,6 @@
+import React, { Component } from 'react';
+
+function Login() {
+  return <div>Login</div>;
+}
+export default Login;
